20d8b4e8928e6e7859211c1dc58fadfb0fb05b42
Elizaveta Petrenko <mistress.camerawork@yandex.ru>
2023-01-20 16:55:19 +0300

    fix: correct all mistakes of first review

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,20 @@ const popupContainer = document.querySelector('.popup');
 const closeEditButton = document.querySelector('.popup__close-btn');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__string-name');
-let jobInput = document.querySelector('.popup__string-job');
+let nameInput = document.querySelector('.popup__string_input_name');
+let jobInput = document.querySelector('.popup__string_input_job');
 let nameField = document.querySelector('.profile__title');
 let jobField = document.querySelector('.profile__subtitle');
 
 function popupOpen() {
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
   popupContainer.classList.add('popup_opened');
 }
 
 function popupClose() {
   popupContainer.classList.remove('popup_opened');
 }
-
-openEditButton.addEventListener('click', popupOpen);
-closeEditButton.addEventListener('click', popupClose);
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
@@ -26,5 +25,8 @@ function handleFormSubmit (evt) {
 
   popupClose();
 }
+
+openEditButton.addEventListener('click', popupOpen);
+closeEditButton.addEventListener('click', popupClose);
 
 formElement.addEventListener('submit', handleFormSubmit);
